a8f8617d34b831e3ab82c51b4aad06e91483a8b3
cal <calvinsoares@gmail.com>
2024-11-09 11:06:56 -0300

    feat: user types dto

---
+++ src/dto/userDto.ts
@@ -0,0 +1,13 @@
+export type UserDTO = {
+  _id: string;
+  username: string;
+  email: string;
+  role: "admin" | "user";
+};
+
+export type CreateUserDTO = {
+  username: string;
+  email: string;
+  password: string;
+  role: "admin" | "user";
+};
