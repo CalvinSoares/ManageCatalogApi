ea202a910047f702b5bcdca0ff89d9160c7c9950
cal <calvinsoares@gmail.com>
2024-11-09 16:27:06 -0300

    feat: model for catalog with user and products

--- src/models/CatalogModel.ts
+++ src/models/CatalogModel.ts
@@ -1,15 +1,16 @@
 import { Schema, model, type Document, type Types } from "mongoose";
+import type { IProduct } from "./ProductModel";
 
 export interface ICatalog extends Document {
   name: string;
-  products: Types.ObjectId[];
+  products: IProduct[];
   user: Types.ObjectId;
 }
 
 const catalogSchema = new Schema<ICatalog>({
   name: { type: String, required: true },
   products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
-  user: { type: Schema.Types.ObjectId, ref: "User" },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 export const Catalog = model<ICatalog>("Catalog", catalogSchema);
