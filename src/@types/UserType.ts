e322220d8a5f22a8b567c637d47a93828c319211
cal <calvinsoares@gmail.com>
2024-11-09 11:08:34 -0300

    feat: user typeRequest to middleware

---
+++ src/@types/UserType.ts
@@ -0,0 +1,10 @@
+import type { Request } from "express";
+
+export type TypeUserToken = {
+  _id: number;
+  username: string;
+  email: string;
+  role: string;
+};
+
+export type TypeRequestUser = Request & { user?: TypeUserToken };
