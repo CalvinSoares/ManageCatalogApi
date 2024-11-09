4bf33572b0343a3ba1de9169a6220a9af8efea96
cal <calvinsoares@gmail.com>
2024-11-09 11:06:10 -0300

    feat: signIn to creating token and signUp

---
+++ src/auth/AuthController.ts
@@ -0,0 +1,75 @@
+import { User } from "../models/UserModel";
+import UserService from "../services/UserService";
+import type { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+class AuthController {
+  async signUp(req: Request, res: Response) {
+    const user = req.body;
+    try {
+      const userCreated = await UserService.add(user);
+      if (!userCreated) {
+        res.status(409).json({ error: "O cadastro já existe" });
+        return;
+      }
+      res.status(201).json({ message: "User created successfully" });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Erro ao registrar usuário", error: err });
+    }
+  }
+
+  async signIn(req: Request, res: Response) {
+    const { email, password } = req.body;
+    console.log("Email recebido:", email);
+    console.log("Senha recebida:", password);
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+      }
+
+      console.log("Hash armazenado no banco:", user.password);
+
+      console.log("senha enviada", password);
+      try {
+        console.log("");
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        console.log("Resultado da validação da senha:", isPasswordValid);
+
+        if (!isPasswordValid) {
+          res.status(401).json({ msg: "Invalid credentials" });
+          return;
+        }
+      } catch (err) {
+        console.log("deu ruim", err);
+      }
+
+      if (process.env.JWT_SECRET === undefined) {
+        res
+          .status(500)
+          .json({ msg: "Variável de ambiente Secret não definida" });
+        return;
+      }
+
+      const token = jwt.sign(
+        {
+          userName: user.username,
+          email: user.email,
+          role: user.role,
+        },
+        process.env.JWT_SECRET
+      );
+
+      res.status(200).json({ token });
+    } catch (err) {
+      res.status(500).json({ message: "Erro ao fazer login", error: err });
+    }
+  }
+}
+
+export default new AuthController();
