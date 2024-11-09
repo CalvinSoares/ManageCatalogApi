35d923349b1f79a40bf21da1ce90c3ab20465ec6
cal <calvinsoares@gmail.com>
2024-11-09 11:06:38 -0300

    feat: middleware for authentication based in roles

---
+++ src/auth/AuthorizeRoles.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Response } from "express";
+import { TypeRequestUser } from "../@types/UserType";
+
+export const authrorizeRole = (allowedRoles: string[]) => {
+  return (req: TypeRequestUser, res: Response, next: NextFunction) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      res.status(403).json({ error: "Acesso negado " });
+    }
+    next();
+  };
+};
