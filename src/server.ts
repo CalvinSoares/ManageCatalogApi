5d6d661dd02a84887928798193b9d3a4525a89d6
cal <calvinsoares@gmail.com>
2024-11-09 16:23:19 -0300

    feat: use routes product and catalog

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { startDB } from "./db/mongoDB";
 import cors from "cors";
 import userRouter from "./routes/UserRoutes";
+import productRouter from "./routes/ProductRoutes";
+import catalogRouter from "./routes/CatalogRoutes";
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(express.urlencoded({ limit: "90mb", extended: true }));
 app.use(cors());
 app.use(express.json({ limit: "90mb" }));
 app.use(userRouter);
+app.use(productRouter);
+app.use(catalogRouter);
 
 startDB();
 
